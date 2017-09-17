5220f5dde2ccf22da41fb004d16aaa7b6dbf1257
KroosT <anton.shestak.96@gmail.com>
2017-09-17 15:55:12 +0300

    Add jelly toolbar, retrofit client, api interface. Test connection and obtaining data passed successful.

---
+++ app/src/main/java/by/kroos/gifsearch/ApiService.java
@@ -0,0 +1,8 @@
+package by.kroos.gifsearch;
+
+/**
+ * Created by anton on 17.09.2017.
+ */
+
+public class ApiService {
+}
---
+++ app/src/main/java/by/kroos/gifsearch/Data.java
@@ -0,0 +1,32 @@
+package by.kroos.gifsearch;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by anton on 17.09.2017.
+ */
+
+class GIF {
+
+    @SerializedName("username")
+    private String author;
+
+    @SerializedName("url")
+    private String url;
+
+    @SerializedName("id")
+    private String id;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
